bb7ef75505978cc99597ee4d1aff6e396d1d9f62
Ashish <ashish.raghav@outworx.com>
2026-09-24 16:39:10 +0530

    price changed

--- app/main.py
+++ app/main.py
@@ -14,7 +14,7 @@ class Item(BaseModel):
 
 # in-memory "database" (just for demo purposes)
 items_db: List[Item] = [
-    Item(id=1, name="Laptop", price=57000.0),
+    Item(id=1, name="Laptop", price=69000.0),
     Item(id=2, name="Mouse", price=500.0),
 ]
 
